64d92e9f47c8b32e65bc08b8a7f3a4c87537b1e6
enpassio <abhishekraj@abhishekraj.me>
2019-01-04 02:07:29 +0530

    Added new restaurant through a form

--- Full Stack Foundations from Udacity/Lesson 2 - Making a Web Server/Objective1WebServer.py
+++ Full Stack Foundations from Udacity/Lesson 2 - Making a Web Server/Objective1WebServer.py
@@ -8,6 +8,20 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -68,6 +82,25 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    engine = create_engine('sqlite:///restaurantmenu.db')
+                    Base.metadata.bind = engine
+                    DBSession = sessionmaker(bind=engine)
+                    session = DBSession()
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+            '''
             self.send_response(301)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
@@ -80,10 +113,11 @@ class webServerHandler(BaseHTTPRequestHandler):
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "'<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'"
             output += "</body></html>"
             self.wfile.write(output)
             print output
+            '''
         except:
             pass
 
