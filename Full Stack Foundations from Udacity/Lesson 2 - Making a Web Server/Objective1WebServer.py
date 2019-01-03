faba901db1e3557c03956efa1e84ec28c75df5af
enpassio <abhishekraj@abhishekraj.me>
2019-01-04 04:14:46 +0530

    Edited restaurant name on clicking edit link

--- Full Stack Foundations from Udacity/Lesson 2 - Making a Web Server/Objective1WebServer.py
+++ Full Stack Foundations from Udacity/Lesson 2 - Making a Web Server/Objective1WebServer.py
@@ -47,6 +47,27 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                engine = create_engine('sqlite:///restaurantmenu.db')
+                Base.metadata.bind = engine
+                DBSession = sessionmaker(bind = engine)
+                session = DBSession()
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = "<html><body>"
+                    output += "<h1>"
+                    output += myRestaurantQuery.name
+                    output += "</h1>"
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                    output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                    output += "<input type = 'submit' value = 'Rename'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -63,7 +84,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 for item in items:
                     contents += item.name
                     contents += "</br>"
-                    contents += "<a href '#'> Edit</a>"
+                    contents += "<a href ='/restaurants/%s/edit' >Edit </a> " % item.id
                     contents += "</br>"
                     contents += "<a href '#'> Delete</a>"
                     contents += "</br>"
@@ -82,6 +103,27 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+                    engine = create_engine('sqlite:///restaurantmenu.db')
+                    Base.metadata.bind = engine
+                    DBSession = sessionmaker(bind = engine)
+                    session = DBSession()
+                    myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(
                     self.headers.getheader('content-type'))
@@ -99,7 +141,6 @@ class webServerHandler(BaseHTTPRequestHandler):
                     self.send_header('Content-type', 'text/html')
                     self.send_header('Location', '/restaurants')
                     self.end_headers()
-
             '''
             self.send_response(301)
             self.send_header('Content-type', 'text/html')
@@ -118,6 +159,7 @@ class webServerHandler(BaseHTTPRequestHandler):
             self.wfile.write(output)
             print output
             '''
+
         except:
             pass
 
