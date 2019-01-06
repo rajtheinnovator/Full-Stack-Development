3d957310052778c8c743900679fb7acf325e2df3
enpassio <abhishekraj@abhishekraj.me>
2019-01-07 01:26:00 +0530

    Functionality to add new menu item implemented

--- Full Stack Foundations from Udacity/Lesson 3 - Developing with frameworks/project.py
+++ Full Stack Foundations from Udacity/Lesson 3 - Developing with frameworks/project.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 #Import render template
 from flask import request, render_template
+#Import reredirect and url_for
+from flask import redirect, url_for
+
 
 app = Flask(__name__)
 # Create session and connect to DB ##
@@ -22,9 +25,16 @@ def restaurantMenu(restaurant_id):
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
@@ -40,5 +50,5 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
